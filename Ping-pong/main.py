653af9f3f94ab39065dc20d3797fa88bb538f14a
Balerik1337 <131802260+Balerik1337@users.noreply.github.com>
2023-05-02 19:28:27 +0300

    v 1.2
    Добавлены картинки и их отображение. Так же движение ракеток.

--- Ping-pong/main.py
+++ Ping-pong/main.py
@@ -17,16 +17,27 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image,(self.rect.x,self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update_l(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < 620:
-            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 350:
+            self.rect.y += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 350:
+            self.rect.y += self.speed
 
 
 window = display.set_mode((700,500))
-window.fill((0,20,60))
+window.fill((40,100,200))
+
+
+racket = Player("racket.png",5,150,50,150,7.5)
+racket2 = Player("racket.png",645,150,50,150,7.5)
+ball = GameSprite("tenis_ball.png",300,200,50,50,7.5)
 
 
 clock = time.Clock()
@@ -36,8 +47,19 @@ finish = False
 
 
 while game:
+    window.fill((40,100,200))
+    racket.update_l()
+    racket2.update_r()
+
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+
+    racket.reset()
+    racket2.reset()
+    ball.reset()
+
+
     display.update()
     clock.tick(FPS)
